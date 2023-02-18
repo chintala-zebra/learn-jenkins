6c5681bfa9d8cfd042cdd9da56031020ffeb2813
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-02-18 12:15:25 -0500

    logger

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,9 +1,13 @@
 #!/usr/bin/env groovy
 
 def info(message) {
-    echo "\033[31m INFO: ${message} \033[0m "
+    echo "\033[34mINFO: ${message}\033[0m"
 }
 
 def warning(message) {
-    echo "WARNING: ${message}"
+    echo "\033[0;33mWARNING: ${message}\033[0m"
+}
+
+def error(message) {
+    echo "\033[31mERROR: ${message}\033[0m"
 }
