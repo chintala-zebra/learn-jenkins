d12f4d103cfa3e380ab85ec7cb9d8aa0cc0f4058
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-19 22:36:42 -0400

    Update upload.groovy

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -1,13 +1,14 @@
+import java.io.File 
+
 def showContent() {
     log.info("The file content that you are going to copy is")
     log.info("=============================================================")
-    log.info(${params.file})
     log.info("=============================================================")
 }
 
 
 def copyFile() {
-    echo ${env.ANSIBLE_VALUT} 
+   // echo "${env.ANSIBLE_VALUT}" > .mysecret
 }
 
 return this
