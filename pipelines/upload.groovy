bf06e852b3e50e2d0b8d86e9a76bf61d64cfafe4
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-19 21:58:06 -0400

    ha ha

---
+++ pipelines/upload.groovy
@@ -0,0 +1,7 @@
+def showContent() {
+    log.info("The file content that you are going to copy is")
+    log.info("=============================================================")
+    log.info("=============================================================")
+}
+
+return this
