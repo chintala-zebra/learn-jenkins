6b0483ed27bb8ea5669a0d4b44789109307f6d92
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-19 21:48:37 -0400

    Update newway.groovy

--- pipelines/newway.groovy
+++ pipelines/newway.groovy
@@ -1,2 +1,11 @@
+def planApp() {
+     echo "Planning code goes here..."
+}
+def testApp() {
+    echo "Testing code goes here..."
+}
+def deployApp() {
+    echo "Deploy code goes here..."
+}
 
 return this
