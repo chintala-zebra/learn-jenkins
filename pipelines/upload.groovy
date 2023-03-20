a93996a0584235e9cd3948cbc85edbbb905d65fb
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-19 22:08:30 -0400

    aaa

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -1,7 +1,23 @@
 def showContent() {
     log.info("The file content that you are going to copy is")
     log.info("=============================================================")
+    if [[ `du -k "$file" | cut -f1` -lt 20 ]]; then
+                                cat $file
+                            else
+                                echo "file is too large"
+                            fi
+
     log.info("=============================================================")
+}
+
+
+def copyFile() {
+    echo $ANSIBLE_VALUT > .mysecret
+    export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+    ansible-vault decrypt the-key
+    echo "removed the key" > .mysecret
+    scp -o 'StrictHostKeyChecking no' -i the-key $file $host_name:$target_file_path
+    echo "\033[32m Copy of file to Host : $host_name @ Path : $target_file_path is successful! \033[0m"
 }
 
 return this
