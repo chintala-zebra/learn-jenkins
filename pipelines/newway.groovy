12c4f9ebc412c7444077174c632f0a05f0cb799d
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-21 13:09:36 -0400

    params setup stage

--- pipelines/newway.groovy
+++ pipelines/newway.groovy
@@ -1,3 +1,13 @@
+def setupParams(){
+    properties([
+        parameters([
+            choice(
+                choices: ['ONE', 'TWO'], 
+                name: 'PARAMETER_01'
+            )
+        ])
+    ])
+}
 def planApp() {
      echo "Planning code goes here..."
 }
