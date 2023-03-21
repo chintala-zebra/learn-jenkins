9bb67661aeccae2ccedc73f8de4745bc98874e09
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-20 22:27:51 -0400

    Update upload.groovy

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -22,7 +22,15 @@ def base64Decode(encodedString){
 }
 
 def copyFile() {
-   sh 'echo "${env.ANSIBLE_VALUT}" > .mysecret'
+                    sh '''
+                            set +x
+                            echo $ANSIBLE_VALUT > .mysecret
+                            export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+                            ansible-vault decrypt the-key
+                            echo "removed the key" > .mysecret
+                            scp -o 'StrictHostKeyChecking no' -i the-key $file $host_name:$target_file_path
+                            echo "\033[32m Copy of file to Host : $host_name @ Path : $target_file_path is successful! \033[0m"
+                    '''
 }
 
 return this
