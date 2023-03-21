a4be282d130ee3a4bb9690e696203d68e600921b
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-21 13:43:06 -0400

    Update newway.groovy

--- pipelines/newway.groovy
+++ pipelines/newway.groovy
@@ -1,10 +1,28 @@
 def setupParams(){
     properties([
         parameters([
-            choice(
-                choices: ['ONE', 'TWO'], 
-                name: 'PARAMETER_01'
-            )
+            [$class: 'ChoiceParameter', 
+                choiceType: 'PT_SINGLE_SELECT', 
+                description: 'Select the Environemnt from the Dropdown List', 
+                filterLength: 1, 
+                filterable: false, 
+                name: 'Env', 
+                script: [
+                    $class: 'GroovyScript', 
+                    fallbackScript: [
+                        classpath: [], 
+                        sandbox: false, 
+                        script: 
+                            "return['Could not get The environemnts']"
+                    ], 
+                    script: [
+                        classpath: [], 
+                        sandbox: false, 
+                        script: 
+                            "return['dev','stage','prod']"
+                    ]
+                ]
+            ]
         ])
     ])
 }
