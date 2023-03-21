ca66383c677e03129bb42144b5eb67c3c645f2fd
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-20 22:24:56 -0400

    Update upload.groovy

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -3,12 +3,26 @@ import java.io.File
 def showContent() {
     log.info("The file content that you are going to copy is")
     log.info("=============================================================")
+    def content = base64Decode (params.file)
+    writeFile(file: "fileName", text: content)
+    sh '''
+    if [[ `du -k "${WORKSPACE}/fileName" | cut -f1` -lt 20 ]]; then
+        cat ${WORKSPACE}/fileName
+    else
+        echo "file is too large"
+    fi
+    '''
     log.info("=============================================================")
 }
 
+def base64Decode(encodedString){
+    byte[] decoded = encodedString.decodeBase64()
+    String decode = new String(decoded)
+    return decode
+}
 
 def copyFile() {
-   // echo "${env.ANSIBLE_VALUT}" > .mysecret
+   sh 'echo "${env.ANSIBLE_VALUT}" > .mysecret'
 }
 
 return this
