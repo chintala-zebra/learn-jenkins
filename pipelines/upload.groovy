2513f8a1a88c39fb32db5c1ae2c6cdc4bb4c3781
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-20 22:36:53 -0400

    Update upload.groovy

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -6,11 +6,12 @@ def showContent() {
     def content = base64Decode (params.file)
     writeFile(file: "fileName", text: content)
     sh '''
-    if [[ `du -k "${WORKSPACE}/fileName" | cut -f1` -lt 20 ]]; then
-        cat ${WORKSPACE}/fileName
-    else
-        echo "file is too large"
-    fi
+        set +x
+        if [[ `du -k "${WORKSPACE}/fileName" | cut -f1` -lt 20 ]]; then
+            cat ${WORKSPACE}/fileName
+        else
+            echo "file is too large"
+        fi
     '''
     log.info("=============================================================")
 }
@@ -22,15 +23,15 @@ def base64Decode(encodedString){
 }
 
 def copyFile() {
-                    sh '''
-                            set +x
-                            echo $ANSIBLE_VALUT > .mysecret
-                            export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
-                            ansible-vault decrypt the-key
-                            echo "removed the key" > .mysecret
-                            scp -o 'StrictHostKeyChecking no' -i the-key ${WORKSPACE}/fileName $host_name:$target_file_path
-                            echo "\033[32m Copy of file to Host : $host_name @ Path : $target_file_path is successful! \033[0m"
-                    '''
+    sh '''
+            set +x
+            echo $ANSIBLE_VALUT > .mysecret
+            export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+            ansible-vault decrypt the-key
+            echo "removed the key" > .mysecret
+            scp -o 'StrictHostKeyChecking no' -i the-key ${WORKSPACE}/fileName $host_name:$target_file_path
+            echo "\033[32m Copy of file to Host : $host_name @ Path : $target_file_path is successful! \033[0m"
+    '''
 }
 
 return this
