9f5504375ca514085e6b4bb1e9cd73c137675906
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-22 14:29:39 -0400

    Update upload.groovy

--- pipelines/upload.groovy
+++ pipelines/upload.groovy
@@ -19,13 +19,12 @@ def setupParams(){
     ])
 }
 
-def areParamsValid(){
+def validateParams(){
     if(params.target_file_path == "" || params.file == "" || params.SERVER == ""){
         currentBuild.result = 'FAILURE'
         error "Required Parameters are empty so, skipping execution."
-        return false
     } else {
-        return true
+        log.info "All Parameters are valid."
     }
 }
 
@@ -36,35 +35,31 @@ def base64Decode(encodedString){
 }
 
 def showContent() {
-    if(areParamsValid()){
-        log.info("The file content that you are going to copy is")
-        log.info("=============================================================")
-        def content = base64Decode (params.file)
-        writeFile(file: "fileName", text: content)
-        sh '''
-            set +x
-            if [[ `du -k "${WORKSPACE}/fileName" | cut -f1` -lt 20 ]]; then
-                cat ${WORKSPACE}/fileName
-            else
-                echo "file is too large"
-            fi
-        '''
-        log.info("=============================================================")
-    }
+    log.info("The file content that you are going to copy is")
+    log.info("=============================================================")
+    def content = base64Decode (params.file)
+    writeFile(file: "fileName", text: content)
+    sh '''
+        set +x
+        if [[ `du -k "${WORKSPACE}/fileName" | cut -f1` -lt 20 ]]; then
+            cat ${WORKSPACE}/fileName
+        else
+            echo "file is too large"
+        fi
+    '''
+    log.info("=============================================================")
 }
 
 def copyFile() {
-    if(areParamsValid()){
-        sh '''
-                set +x
-                echo $ANSIBLE_VALUT > .mysecret
-                export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
-                ansible-vault decrypt the-key
-                echo "removed the key" > .mysecret
-                scp -o 'StrictHostKeyChecking no' -i the-key ${WORKSPACE}/fileName $SERVER:$target_file_path
-        '''
-        log.info ("Copy of file to Host : ${params.SERVER} @ Path : ${params.target_file_path} is successful!")
-    }
+    sh '''
+            set +x
+            echo $ANSIBLE_VALUT > .mysecret
+            export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+            ansible-vault decrypt the-key
+            echo "removed the key" > .mysecret
+            scp -o 'StrictHostKeyChecking no' -i the-key ${WORKSPACE}/fileName $SERVER:$target_file_path
+    '''
+    log.info ("Copy of file to Host : ${params.SERVER} @ Path : ${params.target_file_path} is successful!")
 }
 
 return this
