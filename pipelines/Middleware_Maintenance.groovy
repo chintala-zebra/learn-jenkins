61f2cd3cf4fdd014565fcaf302debfde95eb8351
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-22 17:40:05 -0400

    Update Middleware_Maintenance.groovy

--- pipelines/Middleware_Maintenance.groovy
+++ pipelines/Middleware_Maintenance.groovy
@@ -4,7 +4,7 @@ def setupParams(){
 }
 
 def validateParams(){
-    if(params.ENV_TYPE == null || params.ENV_TYPE == "" || params.CLUSTER_NAME == null || params.CLUSTER_NAME == "" || params.Application == null ||  || params.Application == ""){
+    if(params.ENV_TYPE == null || params.ENV_TYPE == "" || params.CLUSTER_NAME == null || params.CLUSTER_NAME == "" || params.Application == null || params.Application == ""){
         currentBuild.result = 'FAILURE'
         error "Required Parameters are empty so, skipping execution."
     }
