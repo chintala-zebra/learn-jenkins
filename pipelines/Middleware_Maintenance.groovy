47c0235d02a62ab035038975bdd8707b3a29dd8e
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-22 17:59:09 -0400

    Update Middleware_Maintenance.groovy

--- pipelines/Middleware_Maintenance.groovy
+++ pipelines/Middleware_Maintenance.groovy
@@ -59,8 +59,7 @@ def validateParams(){
 }
 
 def setupParameterDisplay() {
-    addShortText(border: 0, text: "ENVIRONMENT:" + ENV_TYPE, background: "azure", color: "black")
-    addShortText(border: 0, text: "CLUSTER_NAME:" + CLUSTER_NAME, background: "beige", color: "black")
+    addShortText(border: 0, text: "ENVIRONMENT:" + ENV_TYPE + "-" + CLUSTER_NAME, background: "azure", color: "black")
     addShortText(border: 0, text: "Application:" + Application, background: "azure", color: "black")
     addShortText(border: 0, text: "JOB_NAME:" + JOB_NAME, background: "beige", color: "black")
 }
