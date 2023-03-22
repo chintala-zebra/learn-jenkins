f47f366ad7b592ccb87f520250ae23e159919ae8
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-22 17:49:23 -0400

    Update Middleware_Maintenance.groovy

--- pipelines/Middleware_Maintenance.groovy
+++ pipelines/Middleware_Maintenance.groovy
@@ -1,6 +1,53 @@
 
 def setupParams(){
-    // Nothing to add here
+        existing = currentBuild.rawBuild.parent.properties
+    .findAll { it.value instanceof hudson.model.ParametersDefinitionProperty }
+    .collectMany { it.value.parameterDefinitions }
+
+    jobParams = existing +     [
+            $class: 'CascadeChoiceParameter',
+            choiceType: 'PT_SINGLE_SELECT',
+            referencedParameters: 'SNOWTICKET, Application',
+            description: """
+            <b>wasCheckProcess</b> - Check how many processes are running on Cluster. <br>
+            <b>appHealth</b> - Check JVM's/Web health. <br>
+            <b>wasBiweeklyMaintenance</b> - Application recycles with/without deployment. <br>
+            <b>wasBiweeklyMaintenanceSTOP</b>- Application stop with/without deployment.<br>
+            <b>wasBiweeklyMaintenanceSTART</b>- Start the application environment.<br>
+            <b>rollingRestartServers</b>- Rolling restarts for the application CLUSTER<br>
+            """,
+            name: 'JOB_NAME',
+            script: [
+                $class: 'GroovyScript',
+                fallbackScript: [ classpath: [], sandbox: true, script: 'return ["ERROR"]' ],
+                script: [
+                    classpath: [],
+                    sandbox: true,
+                    script: '''
+                        if (Application.isEmpty() || Application==""){
+                            return ''
+                        }
+                        else
+                        {                        
+                            def jobs = [
+                                "appHealth",
+                                "wasCheckProcess",
+                                "wasBiweeklyMaintenance",
+                                "wasBiweeklyMaintenanceSTOP",
+                                "wasBiweeklyMaintenanceSTART",
+                                "RWS_Extra_Static_Deployment",
+                                "rollingRestartServers"
+                            ]
+                            return jobs
+                        }
+                    '''
+                ]
+            ]
+        ]
+
+    properties([
+        parameters(jobParams)
+    ])
 }
 
 def validateParams(){
