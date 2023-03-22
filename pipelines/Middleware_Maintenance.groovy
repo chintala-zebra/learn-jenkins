680092a5ad72bc71c0c3541f45da5f9bf1be4062
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-22 17:33:42 -0400

    Final

--- pipelines/Middleware_Maintenance.groovy
+++ pipelines/Middleware_Maintenance.groovy
@@ -4,10 +4,14 @@ def setupParams(){
 }
 
 def validateParams(){
+    if(params.ENV_TYPE == "" || params.CLUSTER_NAME == "" || params.Application == ""){
+        currentBuild.result = 'FAILURE'
+        error "Required Parameters are empty so, skipping execution."
+    }
 }
 
 def executeMainteance() {
-    log.info("Execuring Maintenance on ${params.SERVER} - ${params.CLUSTER_NAME} - ${params.Application}")
+    log.info("Execuring Maintenance on ${params.ENV_TYPE} - ${params.CLUSTER_NAME} - ${params.Application}")
 }
 
 return this
