76628084edb4e71e2d5e6c1e003d9307c2e9a87b
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-23 19:24:27 -0400

    re-org and moved old pipelines

--- pipelines/scripts/newway.groovy
+++ pipelines/scripts/newway.groovy
@@ -1,105 +1,12 @@
-import groovy.io.FileType
-
 
 def setupParams(){
-    properties([
-        parameters([
-            [$class: 'ChoiceParameter',
-                choiceType: 'PT_SINGLE_SELECT',
-                filterable: false,
-                name: 'JobName',
-                script: [$class: 'GroovyScript',
-                    fallbackScript: [ classpath: [], sandbox: true, script: 'return ["ERROR"]' ],
-                    script: [
-                        classpath: [],
-                        sandbox: true,
-                        script: '''
-                            def build = Thread.currentThread().getName()
-                            def regexp= ".+?/job/([^/]+)/.*"
-                            def match = build  =~ regexp
-                            def jobName = match[0][1]
-                            def parts = jobName.split('_');
-                            return [jobName]
-                        '''.stripIndent()
-                    ]
-                ]
-            ]
-            ,[$class: 'CascadeChoiceParameter', 
-                choiceType: 'PT_SINGLE_SELECT', 
-                description: 'Select the Environemnt from the Dropdown List', 
-                name: 'Env', 
-                referencedParameters: 'JobName', 
-                script: [
-                    $class: 'GroovyScript', 
-                    fallbackScript: [ classpath: [], sandbox: true, script: 'return ["ERROR"]' ],
-                    script: [
-                        classpath: [], 
-                        sandbox: true, 
-                        script: '''
-                            import groovy.io.FileType                            
-                            def getAllFolders() {
-                                def list = []
-                                def dir = new File("/application/ansible/inventory/")
-                                dir.eachFile (FileType.DIRECTORIES) { file ->
-                                    list << file.name
-                                }
-                                return list.sort() - 'group_vars' 
-                            }
-                            def parts = JobName.split('_');
-                            if(parts.length > 2){
-                                return [parts[2]]
-                            } else {
-                                return getAllFolders()
-                            }
-                        '''
-                            
-                    ]
-                ]
-            ]
-            ,[$class: 'CascadeChoiceParameter', 
-                choiceType: 'PT_SINGLE_SELECT', 
-                description: 'Select the AMI from the Dropdown List',
-                name: 'Cluster', 
-                referencedParameters: 'JobName,Env', 
-                script: 
-                    [$class: 'GroovyScript', 
-                    
-                    script: [
-                            classpath: [], 
-                            sandbox: true, 
-                            script: ''' 
-                                import groovy.io.FileType                            
-                                def getFoldersUnder(String folderName) {
-                                    def list = []
-                                    list.add('')
-                                    def dir = new File("/application/ansible/inventory/${folderName}/")
-                                    dir.eachFile (FileType.DIRECTORIES) { file ->
-                                        list << file.name
-                                    }
-                                    return list.sort() - 'group_vars' 
-                                }
-                                def parts = JobName.split('_');
-                                if(parts.length > 3){
-                                    return [parts[3]]
-                                } else {
-                                    return getFoldersUnder(Env)
-                                } 
-                            '''
-                            
-                        ]
-                ]
-            ]
-        ])
-    ])
+
 }
-def planApp() {
-     echo "Planning code goes here..."
+def validateParams() {
+     echo "Validating Parameters code goes here..."
 }
-def testApp() {
-    echo "Testing code goes here..."
-}
-def deployApp() {
-    echo "Deploy code goes here..."
+def executeJob() {
+    echo "Execute Job code goes here..."
 }
 
 return this
