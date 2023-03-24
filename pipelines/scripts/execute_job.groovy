1bab06ab893e345d0ca53b277e8fc3d92ccee151
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-24 09:45:13 -0400

    Update execute_job.groovy

--- pipelines/scripts/execute_job.groovy
+++ pipelines/scripts/execute_job.groovy
@@ -9,7 +9,7 @@ def setupParams(){
         string( name: 'command_to_execute', 
             description: """
             Please enter the job that needs executed. - <b> Must be full path to the script </b>
-            <br> Ex: /mount/jobs/executeme.sh'
+            <br> <b>Ex: /mount/jobs/executeme.sh'</b>
             <br> Note: Job will require manual approval if the script is not from <b>/mount or /tmp </b>
             <br> Note: You can't use this to execute <b>rm</b> commands
             """)
