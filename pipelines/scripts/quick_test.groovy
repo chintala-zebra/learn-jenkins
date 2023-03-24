e2d0a3b9936ed6de5190e80752cad08de70b9bb7
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-24 11:59:26 -0400

    test job

---
+++ pipelines/scripts/quick_test.groovy
@@ -0,0 +1,35 @@
+
+def setupParams(){
+    echo "Additioanl Parameters code goes here..."
+}
+
+def validateParams() {
+    setupParameterDisplay()
+     echo "Validating Parameters code goes here..."
+     if(false){
+        currentBuild.result = 'NOT_BUILT'
+        error "Required Parameters are empty so, skipping execution."
+     }
+}
+
+def setupParameterDisplay() {
+    //addShortText(border: 0, text: "ENVIRONMENT:" + ENV_TYPE, background: "azure", color: "black")
+    //addShortText(border: 0, text: "ENVIRONMENT:" + ENV_TYPE, background: "beige", color: "black")
+}
+
+def validateOptionalParams(){
+    echo "Validating Optional Parameters code goes here..."
+}
+
+def executeJob(String jobName) {
+        echo jobName
+
+    sh '''
+
+        echo jobName
+
+    '''
+    echo "Execute Job code goes here..."
+}
+
+return this
