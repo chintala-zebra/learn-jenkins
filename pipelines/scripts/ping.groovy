ce42dad08f119fafbb7a58cf14401ec30838ab10
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-23 20:09:57 -0400

    Update ping.groovy

--- pipelines/scripts/ping.groovy
+++ pipelines/scripts/ping.groovy
@@ -7,15 +7,15 @@ def validateParams() {
 }
 def executeJob() {
    sh """
-                    set +x
-                    echo $ANSIBLE_VALUT > .mysecret
-                    export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
-                    ansible-vault decrypt the-key
-                    echo "removed the key" > .mysecret
+     set +x
+     echo $ANSIBLE_VALUT > .mysecret
+     export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+     ansible-vault decrypt the-key
+     echo "removed the key" > .mysecret
 
-                    export ANSIBLE_HOST_KEY_CHECKING=False
-                    export ANSIBLE_FORCE_COLOR=true
-                    ansible-playbook -i hosts/hosts.yaml ping-play.yaml
+     export ANSIBLE_HOST_KEY_CHECKING=False
+     export ANSIBLE_FORCE_COLOR=true
+     ansible-playbook -i hosts/hosts.yaml ping-play.yaml
     """
 }
 
