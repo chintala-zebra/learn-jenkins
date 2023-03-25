f22c0ef229e1a6296cacedc2c7a33c01d86beb43
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-25 13:40:37 -0400

    Update utilities.groovy

--- pipelines/libraries/utilities.groovy
+++ pipelines/libraries/utilities.groovy
@@ -14,15 +14,9 @@ def notifyMiddleware(){
     log.info "Notifying Middleware team of Failure."
     office365ConnectorSend webhookUrl: MIDDLEWARE_TEAMS_CHANNEL,
         factDefinitions: [
-            if(params.ENV_TYPE != "" ){
                 [name: "Environment", template:  ENV_TYPE],
-            }
-            if(params.CLUSTER_NAME != "" ){
                 [name: "Cluster", template: CLUSTER_NAME],
-            }
-            if(params.Application != "" ){
                 [name: "Application", template: Application]                            
-            }
         ]
 }
 
