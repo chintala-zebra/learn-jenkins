9039f60feab7945fe430681c8fd796dd743fae1d
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-25 21:12:58 -0400

    Update ansible_helper.groovy

--- pipelines/libraries/ansible_helper.groovy
+++ pipelines/libraries/ansible_helper.groovy
@@ -1,12 +1,18 @@
-def execute_simple_playbook(String inventory, String playbook) {
-    log.info("Executing Playbook ${playbook} on inventory ${inventory}")
+def setupSSHKeys() {
     sh """
         set +x
         echo $ANSIBLE_VALUT > .mysecret
         export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
         ansible-vault decrypt the-key
         echo "removed the key" > .mysecret
+    """
+}
 
+
+def execute_simple_playbook(String inventory, String playbook) {
+    log.info("Executing Playbook ${playbook} on inventory ${inventory}")
+    setupSSHKeys()
+    sh """
         export ANSIBLE_HOST_KEY_CHECKING=False
         export ANSIBLE_FORCE_COLOR=true
         ansible-playbook -i "${inventory}" "${playbook}"
