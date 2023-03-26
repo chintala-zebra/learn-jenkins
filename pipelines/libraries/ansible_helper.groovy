6393ce825ce2a0668069bca8f3a131643754eab4
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-25 20:08:00 -0400

    Update ansible_helper.groovy

--- pipelines/libraries/ansible_helper.groovy
+++ pipelines/libraries/ansible_helper.groovy
@@ -1,16 +1,17 @@
 def execute_simple_playbook(String inventory, String playbook) {
-    def my_inventory = inventory
-    def my_playbook = playbook
-   sh """
-    echo "${my_inventory}"
-    //  set +x
-    //  echo $ANSIBLE_VALUT > .mysecret
-    //  export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
-    //  ansible-vault decrypt the-key
-    //  echo "removed the key" > .mysecret
+    log.info("Executing Playbook ${playbook} on inventory ${inventory}")
+    sh """
+        set +x
+        echo $ANSIBLE_VALUT > .mysecret
+        export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+        ansible-vault decrypt the-key
+        echo "removed the key" > .mysecret
 
-    //  export ANSIBLE_HOST_KEY_CHECKING=False
-    //  export ANSIBLE_FORCE_COLOR=true
-//     ansible-playbook -i "${inventory}" "${playbook}"
+        export ANSIBLE_HOST_KEY_CHECKING=False
+        export ANSIBLE_FORCE_COLOR=true
+        ansible-playbook -i "${inventory}" "${playbook}"
     """
+    log.info("Playbook ${playbook} execution on inventory ${inventory} is completed successfully.")
 }
+
+return this
