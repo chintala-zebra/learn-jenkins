cf2b58d20b5cef068901efe3bcf38f7b1459443a
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-25 20:03:34 -0400

    Update ansible_helper.groovy

--- pipelines/libraries/ansible_helper.groovy
+++ pipelines/libraries/ansible_helper.groovy
@@ -1,13 +1,16 @@
 def execute_simple_playbook(String inventory, String playbook) {
+    def my_inventory = inventory
+    def my_playbook = playbook
    sh """
-     set +x
-     echo $ANSIBLE_VALUT > .mysecret
-     export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
-     ansible-vault decrypt the-key
-     echo "removed the key" > .mysecret
+    echo "${my_inventory}"
+    //  set +x
+    //  echo $ANSIBLE_VALUT > .mysecret
+    //  export ANSIBLE_VAULT_PASSWORD_FILE=.mysecret
+    //  ansible-vault decrypt the-key
+    //  echo "removed the key" > .mysecret
 
-     export ANSIBLE_HOST_KEY_CHECKING=False
-     export ANSIBLE_FORCE_COLOR=true
-     ansible-playbook -i "$inventory" "$playbook"
+    //  export ANSIBLE_HOST_KEY_CHECKING=False
+    //  export ANSIBLE_FORCE_COLOR=true
+//     ansible-playbook -i "${inventory}" "${playbook}"
     """
 }
