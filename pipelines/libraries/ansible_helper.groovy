b3309c876c51bdbba3f31ba54a4293a0f6b65cb0
Prasanna Chintala <80276358+chintala-zebra@users.noreply.github.com>
2023-03-30 10:38:17 -0400

    vars as parameter

--- pipelines/libraries/ansible_helper.groovy
+++ pipelines/libraries/ansible_helper.groovy
@@ -9,19 +9,31 @@ def setupSSHKeys() {
 }
 
 def execute_playbook(String inventory, String playbook) {
-    execute_playbook_on_host(inventory,"*",playbook)
+    execute_playbook_on_host_with_vars(inventory, "*", playbook, "")
+}
+
+def execute_playbook_with_vars(String inventory, String playbook, String extraVars) {
+    execute_playbook_on_host_with_vars(inventory,"*", playbook, extraVars)
 }
 
 def execute_playbook_on_host(String inventory, String hostNames, String playbook) {
-    log.info("Executing Playbook ${playbook} on inventory ${inventory} with host(s) ${hostNames}")
+    execute_playbook_on_host_with_vars(inventory, hostNames, playbook, "")
+}
+
+def execute_playbook_on_host_with_vars(String inventory, String hostNames, String playbook, String extraVars) {
+    log.info("Executing Playbook ${playbook} on inventory ${inventory} with host(s) ${hostNames} and extravars ${extraVars}")
     setupSSHKeys()
     sh """
         set +x
         export ANSIBLE_HOST_KEY_CHECKING=False
         export ANSIBLE_FORCE_COLOR=true
-        ansible-playbook -i "${inventory}" -e "HOSTS=${hostNames}" "${playbook}" --private-key appadmin-key
+        if(extraVars == ""){
+            ansible-playbook -i "${inventory}" -e "HOSTS=${hostNames}" "${playbook}" --private-key appadmin-key
+        }else {
+            ansible-playbook -i "${inventory}" -e "HOSTS=${hostNames}" "${playbook}" extraVars --extra-vars="${extraVars}" --private-key appadmin-key 
+        }
     """
-    log.info("Playbook ${playbook} execution on inventory ${inventory} with host(s) ${hostNames} completed successfully.")
+    log.info("Playbook ${playbook} execution on inventory ${inventory} with host(s) ${hostNames} and extravars ${extraVars} completed successfully.")
 }
 
 return this
---
+++ pipelines/scripts/ansible_playbook_extravars.groovy
@@ -0,0 +1,48 @@
+
+def setupParams(){
+    // Get params from helper
+    params_helper = load "pipelines/libraries/env_params_helper.groovy"
+    helperParams = params_helper.getInventoryParamsUptoApplication()
+    //log.info("Additioanl Parameters code goes here...")
+    // Add additional params
+    jobParams = [
+        choice(name: 'PLAY_BOOK', choices: ['jvm_restart.yml','liberty_install.yml','execute-shell.yaml','roles-sample.yml','show-ipaddress.yaml','whoami-playbook.yml'], description: 'Playbook to Execute'),
+            jobParams = [
+        string( name: 'extra_vars', 
+            description: """
+            Extra variabled that needs passed to the playbook. - <b> sample: name:value </b>
+            """)
+    ]
+    //setup Parameters to Job
+    params_helper.setupParams(helperParams + jobParams)
+}
+
+def validateParams() {
+    setupParameterDisplay()
+    if(params.ENV_TYPE == "" || params.CLUSTER_NAME == "" || params.Application == "" || params.PLAY_BOOK == ""){
+        currentBuild.result = 'NOT_BUILT'
+        error "Required Parameters are empty so, skipping execution."
+    }
+}
+
+def setupParameterDisplay() {
+    if(params.ENV_TYPE != "" ){
+        addShortText(border: 0, text: "Environment:" + ENV_TYPE, background: "azure", color: "black")
+    }
+    if(params.CLUSTER_NAME != "" ){
+        addShortText(border: 0, text: "Cluster:" + CLUSTER_NAME , background: "beige", color: "black")
+    }
+    if(params.Application != "" ){
+        addShortText(border: 0, text: "Application:" + Application, background: "azure", color: "black")
+    }
+    if(params.PLAY_BOOK != "" ){
+        addShortText(border: 0, text: "Play Book:" + PLAY_BOOK, background: "azure", color: "black")
+    }
+}
+
+def executeJob() {
+    ansible_helper = load "pipelines/libraries/ansible_helper.groovy"
+    ansible_helper.execute_playbook("/application/ansible/inventory/${ENV_TYPE}/${CLUSTER_NAME}/${Application}","${WORKSPACE}/ansible/${PLAY_BOOK}")
+}
+
+return this
